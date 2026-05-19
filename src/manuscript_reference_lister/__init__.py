84f49b78e5641ddb3dbcedd419aa975257791134
Sebastien Lenard <sebastien.lenard@gmail.com>
2026-05-18 20:06:54 -0600

    [debug] __init__: removed JournalSyncException

--- src/manuscript_reference_lister/__init__.py
+++ src/manuscript_reference_lister/__init__.py
@@ -1,8 +1,6 @@
 from .cli import main
-from .exceptions import JournalSyncError
 
 # Warning: don't include packages that can call themselves in a circular way
 __all__ = [
-    "JournalSyncError",
     "main",
 ]
