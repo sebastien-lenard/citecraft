8c2370ba80a059a11f853812b6c6baf0f039f038
Sebastien Lenard <sebastien.lenard@gmail.com>
2026-05-29 19:39:43 -0600

    work_repository -> crossref_work_repository script mv

--- src/manuscript_reference_lister/repositories/__init__.py
+++ src/manuscript_reference_lister/repositories/__init__.py
@@ -1,8 +1,8 @@
 from .base_repository import BaseRepository
+from .crossref_work_repository import WorkRepository
 from .doi_repository import DoiRepository
 from .journal_repository import JournalRepository
 from .style_repository import StyleRepository
-from .work_repository import WorkRepository
 
 # Warning: don't include packages that can call themselves in a circular way
 __all__ = [
